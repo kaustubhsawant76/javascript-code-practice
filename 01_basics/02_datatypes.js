2a241b97d2c2836d454b3e7f8183981629f9c6a8
kaustubhsawant76 <145922083+kaustubhsawant76@users.noreply.github.com>
2024-02-08 13:05:49 +0000

    datatype basics

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict mode" //treat all code as newer version
+//alert used in browser doesnt work in node js
+
+//DATATYPES ARE=
+n2b2s2u
+null
+Number
+Boolean
+BigInt
+String
+Symbol
+undefined
