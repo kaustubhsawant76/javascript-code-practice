0345dcfd5ab29dadea621dfbf969e848020df795
kaustubhsawant76 <145922083+kaustubhsawant76@users.noreply.github.com>
2024-02-09 03:08:28 +0000

    datatype code with harry

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,21 @@
+//CODE WITH HARRY
+
+let score=33
+
+
+score=String(33)
+console.log(typeof score)
+
+score=Number(33)
+console.log(typeof score)
+
+score=parseInt(33)
+console.log(typeof score)
+
+score=parseFloat(33)
+console.log(typeof score)
+
+
+//CHAI AND CODE 
+
+
