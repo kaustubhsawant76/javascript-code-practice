88e49cd2d53fd5ebf763ca42ddc57035be3dfaa0
kaustubhsawant76 <145922083+kaustubhsawant76@users.noreply.github.com>
2024-02-17 07:09:51 +0000

    chai and code datatypes

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -17,5 +17,16 @@ console.log(typeof score)
 
 
 //CHAI AND CODE 
+let scores="33amxdk";
+console.log(typeof scores);
+console.log(typeof (scores));
+let number1=Number(scores);
+console.log(typeof number1)
+console.log(number1)
+//string "33amzdk" converts its type to number but the actulal number value is NaN ie not a numer
+//null to number conersion is 0
+//undefined conversion to number is NaN ie not a number
+//true coverts 1 false converts 0
+
 
 
