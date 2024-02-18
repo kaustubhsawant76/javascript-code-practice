1c9c8d367967c5c5b5a3003a6740b88fa5c69037
kaustubhsawant76 <145922083+kaustubhsawant76@users.noreply.github.com>
2024-02-18 14:19:08 +0000

    cwh problemset 1

---
+++ 01_basics/cwhps1.js
@@ -0,0 +1,27 @@
+//Q1
+let a="kaustubh"
+console.log(typeof a)
+a=4
+console.log(a)
+//Q2
+console.log(typeof a)
+//Q3
+const object=
+{
+ kaustubh:30,
+ vrushabh:20,
+ sawant:"abc",
+}
+// object=3;
+//Q4
+object['babaji']="45"
+console.table(object)
+//Q5
+const dictionary=
+{
+  zenith:"the highest point reached by  heavenly body",
+  zap:"to destroy hit or kill somebody",
+  xenophobia: "fear or hatred of foreign people and cultures",  
+
+}
+console.log(dictionary)
