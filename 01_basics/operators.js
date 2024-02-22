b1b54f763dcf7bdf50cd1a6357efee8aaf45bbc4
kaustubhsawant76 <145922083+kaustubhsawant76@users.noreply.github.com>
2024-02-22 05:37:35 +0000

    operators cwh arithmetic

---
+++ 01_basics/operators.js
@@ -0,0 +1,10 @@
+//CODE WITH HARRY
+//ARITHMETIC OPERATORS
+let a=10;
+let b=20;
+console.log("the addition of a+b is ",a+b)
+console.log("the subtraction of a+b is ",a-b)
+console.log("the division of a+b is ",a/b)
+console.log("the multiplicatin of a+b is ",a*b)
+console.log("the exponentional of a+b is ",a**b)
+console.log("the modulus of a+b is ",a%b)
