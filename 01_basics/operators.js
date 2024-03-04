3f716a6a4a2071abbc8c4c50e5970cddc099a94b
kaustubhsawant76 <145922083+kaustubhsawant76@users.noreply.github.com>
2024-03-04 08:27:51 +0000

    comparision and logical operators

--- 01_basics/operators.js
+++ 01_basics/operators.js
@@ -3,6 +3,7 @@
 let a=10;
 let b=20;
 console.log("the addition of a+b is ",a+b)
+console.log(a)
 console.log("the subtraction of a+b is ",a-b)
 console.log("the division of a+b is ",a/b)
 console.log("the multiplicatin of a+b is ",a*b)
@@ -25,3 +26,19 @@ console.log("*= output is",c*=5) //50
 console.log("/= output is",c/=5) //10
 console.log("**= output is",c**=2) //100
 console.log("% output is",c%=5)  //0
+
+//COMPARISION OPERATORS
+ let comp1=6;
+ let comp2='6';
+ console.log("comp1==comp2",comp1==comp2)
+ console.log("comp1!=comp2",comp1!=comp2)
+ console.log("comp1===comp2",comp1===comp2)
+ console.log("comp1!==comp2",comp1!==comp2)
+
+ //LOGICAL OPERATORS
+ let x=5
+ let y=6
+ console.log(!false)
+ console.log("x<y && x==5",x<y && x==5)
+ console.log("x>y || x!5",x>y  || x!=5)
+ console.log("x<y || x!y",x>y  || x !=y)
