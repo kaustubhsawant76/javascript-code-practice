57a0a785a930c473561d38f9ca90f69eb7cd66e6
kaustubhsawant76 <145922083+kaustubhsawant76@users.noreply.github.com>
2024-03-09 07:09:13 +0000

    stackand  heap memory

---
+++ 01_basics/stackandheap.js
@@ -0,0 +1,16 @@
+//STACK MEMORY
+let myName="kaustubhsawant"
+let yourName=myName
+myName="rajashree sawant"
+console.log(myName)
+console.log(yourName)
+//HEAP MEMORY
+let obj1=
+{
+   emailOne:"kasutubhsawant76@gmail.com",
+    upId:"ui10",
+}
+let obj2=obj1
+obj2.emailOne="vrushabh.sawant@gmail.com"
+console.table(obj1)
+console.table(obj2)
