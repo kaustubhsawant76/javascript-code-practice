e1d290629a0628289e93847b95c28ad9ad9b72b2
kaustubhsawant76 <145922083+kaustubhsawant76@users.noreply.github.com>
2024-03-12 05:25:14 +0000

    strings

---
+++ 01_basics/strings.js
@@ -0,0 +1,16 @@
+let a="kaustubh"
+console.log(a)
+console.log(a.length)
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a[6])
+console.log(a[7])
+//STRINGS CAN ALSO BE WRITTEN IN '' single quotes
+//STRING LITERALS
+let b="kaustubh"
+let c="sawant"
+console.log(`${b} surname is ${c}`)
