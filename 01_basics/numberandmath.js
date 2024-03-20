97ad83ae9aa1f6f9c563740ad6b3dbfd63d01801
kaustubhsawant76 <145922083+kaustubhsawant76@users.noreply.github.com>
2024-03-20 10:57:19 +0000

    numbers

---
+++ 01_basics/numberandmath.js
@@ -0,0 +1,9 @@
+let a=new Number(123.567)
+
+console.log(a.toString().length);
+console.log(typeof (a))
+console.log(a.toFixed(2));
+let  b=123.567
+console.log(b.toPrecision(3));
+let c=10000000
+console.log(c.toLocaleString('en-in'));
