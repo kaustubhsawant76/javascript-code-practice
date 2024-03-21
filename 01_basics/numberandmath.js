fc3669675542fc494c146f4266e85a08d4613e0c
kaustubhsawant76 <145922083+kaustubhsawant76@users.noreply.github.com>
2024-03-21 12:33:13 +0000

    math

--- 01_basics/numberandmath.js
+++ 01_basics/numberandmath.js
@@ -7,3 +7,20 @@ let  b=123.567
 console.log(b.toPrecision(3));
 let c=10000000
 console.log(c.toLocaleString('en-in'));
+
+
+//MATHS
+console.log(Math.abs(-5));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.8));
+console.log(Math.round(4.6));
+console.log(Math.round(4.4));
+console.log(Math.min(4,6,2,7));
+console.log(Math.max(1,4,9,5,3));
+//Math.random value is always between 0 and 1
+console.log((Math.random()*10)+1);
+console.log((Math.random()*10)+1);
+
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
