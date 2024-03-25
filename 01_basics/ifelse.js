bfa149a7833fcbfd76c8e433ca1fc1dd274b7ed8
kaustubhsawant76 <145922083+kaustubhsawant76@users.noreply.github.com>
2024-03-25 03:20:39 +0000

    if else

---
+++ 01_basics/ifelse.js
@@ -0,0 +1,26 @@
+let a= prompt("enter the age");
+a=Number.parseInt(a);
+console.log(typeof (a));
+if(a>0)
+{alert("this is a valid age")}
+else
+{
+    alert("this is not a valid age")
+}
+
+//IF ELSEIF ELSEIF ELSE
+let b=prompt("enter your age")
+b=Number.parseInt(b);
+if(b>0&&b<18)
+{alert("you are a child and not allowed to drive")
+}
+else if(b>18&&b<20)
+{
+    alert("you are teen and allowed to drive but carefully")
+}
+else if(b>20&&b<60)
+{alert("yor are mature and legally allowed to drive")}
+else
+{
+    alert("you are senior citizen do not drive")
+}
