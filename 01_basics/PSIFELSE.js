688e72e8c384152ac4dae1940bf6a4d666a215ef
kaustubhsawant76 <145922083+kaustubhsawant76@users.noreply.github.com>
2024-03-27 10:18:42 +0000

    PS1,2,5

---
+++ 01_basics/PSIFELSE.js
@@ -0,0 +1,40 @@
+//PS1
+
+// let age=prompt("enter your age")
+// age=Number.parseInt(age)
+// age>10&&age<20?"age lies in the range":"age doesnot lies in the range"
+
+//PS2
+let day ="monday"
+
+switch(day)
+{
+case "monday":
+        console.log("this is monday");
+        break;
+case "tuesday":
+            console.log("this is tuesday");
+            break;
+case "wednesday":
+                console.log("this is wednesday");
+                break;
+case "thursday":
+                    console.log("this is thursday");
+                    break;
+case "friday":
+                        console.log("this is friday");
+                        break;
+case "satday":
+                            console.log("this is saturday");
+                            break;
+case "sunday":
+                                console.log("this is sunday");
+                                break;
+}
+
+//Q5
+let age=prompt("enter your age")
+age=Number.parseInt(age)
+age>18?"you can drive legaly":"you cannot drive legally"
+
+//PS3 
