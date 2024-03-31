f95abeb0b3e4c9862aa8c08d2214fde66357bdea
kaustubhsawant76 <145922083+kaustubhsawant76@users.noreply.github.com>
2024-03-31 11:57:34 +0000

    string basics

---
+++ 01_basics/arrays.js
@@ -0,0 +1,16 @@
+let marks=[70,90,56,false,"kaustubh"]
+console.log(marks);
+console.log(marks[0]);
+console.log(marks[1]);
+console.log(marks[2]);
+console.log(marks[3]);
+console.log(marks[4]);
+console.log(marks[5]);
+console.log(marks[6]);
+marks[6]="vrushabh"
+console.log(marks[6]);
+
+console.log(marks.length);
+console.log(marks.join("/"));
+marks=marks.toString()
+console.log(typeof (marks));
