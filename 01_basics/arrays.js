49c13fae50a7167fabb5297fe7d287ad521a6c98
kaustubhsawant76 <145922083+kaustubhsawant76@users.noreply.github.com>
2024-04-01 09:57:18 +0000

    array methods

--- 01_basics/arrays.js
+++ 01_basics/arrays.js
@@ -11,6 +11,34 @@ marks[6]="vrushabh"
 console.log(marks[6]);
 
 console.log(marks.length);
-console.log(marks.join("/"));
-marks=marks.toString()
-console.log(typeof (marks));
+// console.log(marks.join("/"));
+// marks=marks.toString()
+// console.log(typeof (marks));
+// console.log(marks);
+console.log(marks);
+marks.pop()
+console.log(marks);
+marks.push(78)
+console.log(marks);
+marks.shift()
+console.log(marks);
+marks.unshift("kaustubh")
+console.log(marks);
+
+
+console.log(marks);
+
+
+delete marks[0]
+console.log(marks);
+
+
+let a1=[1,2,3]
+let a2=[4,5,6]
+let a3=[7,8,9]
+let a4=a1.concat(a2,a3)
+console.log(a4);
+let a5=[8,1,6,9,4]
+a5.sort()
+console.log(a5);
+
