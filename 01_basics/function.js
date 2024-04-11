e107f5152f16bc45316f79e741f2f830e2b6fab0
kaustubhsawant76 <145922083+kaustubhsawant76@users.noreply.github.com>
2024-04-11 16:51:59 +0000

    function basics

---
+++ 01_basics/function.js
@@ -0,0 +1,9 @@
+//FUNCTION IS A BLOCK OF CODE WHICH IS WRITEEN SO THAT THERE IS NO NEED OF WRITING CODE REPEATEDLY JUST CALL FUNCTION
+
+function addTwoNumbers(a,b,c,d)
+{
+    console.log(a+b);
+    return c-d;
+}
+let e=addTwoNumbers(19,50,20,10)
+console.log(e);
