9bbe8fad33b099695638635fd6e3e201df6c103d
kaustubhsawant76 <145922083+kaustubhsawant76@users.noreply.github.com>
2024-04-12 07:20:26 +0000

    cwh  function

--- 01_basics/function.js
+++ 01_basics/function.js
@@ -1,9 +1,40 @@
+
+//CODE WITHB HARRY
 //FUNCTION IS A BLOCK OF CODE WHICH IS WRITEEN SO THAT THERE IS NO NEED OF WRITING CODE REPEATEDLY JUST CALL FUNCTION
 
-function addTwoNumbers(a,b,c,d)
-{
+function addTwoNumbers(a,b){
     console.log(a+b);
-    return c-d;
 }
-let e=addTwoNumbers(19,50,20,10)
+addTwoNumbers(30,20)
+
+function subTwoNumbers(c,d)
+{
+    return c-d;
+
+}
+
+let e=console.log(subTwoNumbers(50,30));
 console.log(e);
+
+function avgTwoNumbers(x,y)
+{
+    return 1+(x+y)/2;
+}
+
+
+let f=1
+let g=2
+let h=3
+console.log("the average of f and g is",avgTwoNumbers(f,g));
+console.log("the average of g and h is",avgTwoNumbers(g,h));
+console.log("the average of f and h is",avgTwoNumbers(f,h));
+
+//FUNCTION NEEDS TO BE CALLED FOR EXECUTION OTHERWISE IT WILL NOT BE EXECUTED
+
+//THE BELOW IS ARROW FUNCTION THIS IS TE BEST WAY TO DECLARE A FUNCTION
+const hello=()=>{
+    console.log("my name is kaustubh");
+}
+hello()
+
+//CHAI AND CODE JAVASCRIPT
