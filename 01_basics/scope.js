649bba1d8eb646728b3c8183dd50d1021a199745
kaustubhsawant76 <145922083+kaustubhsawant76@users.noreply.github.com>
2024-04-14 05:02:59 +0000

    scope basics

---
+++ 01_basics/scope.js
@@ -0,0 +1,10 @@
+
+if(true){
+    let a=10
+const b=20
+var c=30
+
+}
+//console.log(a);     //console log will not work out of the block scope  but if we want to print the valye then type console log in block scope
+ //console.log(b);
+ //console.log(c);  //var output comes in global scope even when it is declared in block scope that is why var is not used nowardays to prevent erroes
