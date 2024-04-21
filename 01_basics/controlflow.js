144effc151468ab5e7fc80ec33de2d9ca76845b0
kaustubhsawant76 <145922083+kaustubhsawant76@users.noreply.github.com>
2024-04-21 13:39:32 +0000

    control flow

--- 01_basics/controlflow.js
+++ 01_basics/controlflow.js
@@ -4,3 +4,8 @@
 //TRUTHY VALUES
 //"0",'false',{},[],function(){},"   "
 
+//NULL COALESCING OPERATOR (??):FOR UNDEFINED AND NULL
+// let var1=10??4
+// let var1=null??4
+let var1=undefined??4
+console.log(var1);m
