c90384707a4c2e3cf46331e5e4bdc0a963f1907e
kaustubhsawant76 <145922083+kaustubhsawant76@users.noreply.github.com>
2024-04-22 13:43:33 +0000

    cwh loops

---
+++ 01_basics/loops.js
@@ -0,0 +1,76 @@
+// for (i=0;i<10;i++)
+// {
+//     console.log(i);
+// }
+// //FOR LOOP ALWAYS PRINTS N-1
+
+//PROGRAM TO ADD N NATUTAL NUMBERS
+// let sum=0
+// let n=prompt("enter the number")
+// n=Number.parseInt(n)
+// for(i=0;i<n;i++)
+// {
+//     sum+=(i+1)
+// }
+// console.log("the addition of"+ n +"natural  number is"+ sum);
+
+
+// const obj=
+// {
+//     kaustubh:100,
+//     vrushabh:200,
+//     rajashree:400,
+// }
+// console.log(obj);
+
+//FOR IN LOOP PRINT KEYS OF AN OBJECT IT CAM ALSO BE USED IN ARRAYS
+// for(let a in obj)
+// {console.log("the values for keys " + a+" are"+obj[a] );
+// }
+
+
+//FOR OF LOOP PRINTS ITERATION OF THE INPUT
+// for(let b of "kaustubh")
+// {
+//     console.log(b);
+// }
+
+//console.log(i) will not come because the scope in i is only till curly braces
+
+
+
+//WHILE LOOP
+
+// let n=prompt("enter the number")
+// n=Number.parseInt(n)
+// let i=0;
+// while(i<n)
+// {
+//     console.log(i);
+//     i++;
+// }
+
+//DO WHILE
+// let n=prompt("enter the number")
+// n=Number.parseInt(n)
+
+//IN DO WHILE LOOP FIRST CODE IS EXECUTED AND THEN CONDITION S CHECKED
+// let i=10;
+
+// do
+// {
+//     console.log(i);
+//     i++;
+// }
+// while(i<10)
+
+//CODE WITH HARRY PROBLEM SET
+//PS1 
+
+
+
+
+
+//CHAI AND CODE
+
+
