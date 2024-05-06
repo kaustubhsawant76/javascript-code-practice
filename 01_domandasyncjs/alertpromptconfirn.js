416c2e72ca36d100827ae6727e98c8fd377d0dbf
kaustubhsawant76 <145922083+kaustubhsawant76@users.noreply.github.com>
2024-05-06 10:42:05 +0000

    alert prompt and confirm

---
+++ 01_domandasyncjs/alertpromptconfirn.js
@@ -0,0 +1,24 @@
+// //html code
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+// </head>
+// <body>
+//     <script src="demo.js"></script>
+// </body>
+// </html>
+
+//js code
+alert("enter the value of a")
+let a=prompt("enter value here",200)
+a=Number.parseInt(a)
+confirm("confirn whether you want to write or not")
+if(true){
+document.write(a)
+}
+
+
+document.write("please enter the number")
