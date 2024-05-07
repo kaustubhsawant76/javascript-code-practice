d33e3ba911af0cbfab39a22c127b3ac5a6af5931
kaustubhsawant76 <145922083+kaustubhsawant76@users.noreply.github.com>
2024-05-07 14:50:24 +0000

    alert prompt  problem set

---
+++ 02_domandasyncjs/alertpromptconfirn.js
@@ -0,0 +1,91 @@
+// //html code
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+// </head>
+// <body>
+//     <script src="demo.js"></script>
+// </body>
+// </html>
+
+//js code
+// alert("enter the value of a")
+// let a=prompt("enter value here",200)
+// a=Number.parseInt(a)
+// confirm("confirn whether you want to write or not")
+// if(true){
+// document.write(a)
+// }
+
+
+// document.write("please enter the number")
+
+
+//PROBLEM SET
+
+//PS1
+// let a=prompt("please enter the age")
+// a=Number.parseInt(a)
+// if(a>18)
+//     {
+//         alert("you can drive you are 18+")
+    
+//     }
+
+// else
+// alert("you are minor")
+
+
+//PS2
+// let a=prompt("please enter the age")
+// a=Number.parseInt(a)
+// confirm("you want to see prompt again")
+// a=prompt("reconfirm")
+// if(a>18)
+//     {
+//         alert("you can drive you are 18+")
+    
+//     }
+
+// else
+// alert("you are minor")
+
+
+//PS3
+
+// let a=prompt("please enter the age")
+// a=Number.parseInt(a)
+// confirm("you want to see prompt again")
+// a=prompt("reconfirm")
+// if(a>18)
+//     {
+//         alert("you can drive you are 18+")
+    
+//     }
+// else if(a= a)
+//     {
+//         console.error("the age is negative")
+//     }
+// else
+// alert("you are minor")
+
+
+//PS4
+
+
+// let a=prompt("please enter the age")
+// a=Number.parseInt(a)
+// if(a>4)
+//     {
+//         location.href="https://www.google.com"
+//     }
+//     else
+//     alert("the number is less than 4")
+
+// //PS5
+
+// let b=prompt("enter the colour"); 
+// document.body.style.background=b;
