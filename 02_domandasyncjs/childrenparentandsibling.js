13535f9cb8c3f440b03667d4167d916c56ebf085
kaustubhsawant76 <145922083+kaustubhsawant76@users.noreply.github.com>
2024-05-17 05:11:03 +0000

    parent

--- 02_domandasyncjs/childrenparentandsibling.js
+++ 02_domandasyncjs/childrenparentandsibling.js
@@ -1,4 +1,6 @@
 //CODE WITH HARRY
+
+//CHILDREN
 console.log(document.body.firstChild);//gives first element it can also be a text
 console.log(document.body.lastChild);//gives last element it can also be a text
 console.log(document.body.childNodes);//gives all childnodes
@@ -10,6 +12,7 @@ console.log(document.body.childNodes[document.body.childNodes.length-1]);// ===a
 console.log(document.body.firstElementChild);//gives first element it doesnot gives a text
 console.log(document.body.lastElementChild);//gives last element it doesnot gives a text
 
+//SIBLINGS
 $0  //BY FIRST MOUSE CLICKING ON AN ELEMENT AND THEN TYPING$0 IN CONSOLE WILL RETURN THAT ELEMENT IN OUTPUT
 $1//IT RETURNS THE PREVIOUS ELEMENT OF $0
 document.head.nextSibling //it gives output body as html has 2 children head and body and both are sibling simultaneously
@@ -18,3 +21,10 @@ document.head.nextElementSibling//it doesnot include text or comment nodes
 document.body.previousElementSibling//it doesnot include text or comment nodes
 
 document.body.childNodes[2].nextElementSibling //another way to acces siblings
+
+//PARENT 
+a=document.body.firstChild //put in a variable
+a.parentNode //get the parent node which is body or a text or comment node
+a.parentElement //it only gives parent element not the nodes
+
+
