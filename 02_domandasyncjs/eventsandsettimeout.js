3e45c91727c9e99b0d7d162b1e406f1a0f18ab3f
kaustubhsawant76 <145922083+kaustubhsawant76@users.noreply.github.com>
2024-07-30 10:57:09 +0000

    setTimeout and setInterval

---
+++ 02_domandasyncjs/eventsandsettimeout.js
@@ -0,0 +1,21 @@
+// let a=setTimeout(() => {
+//     console.log("kaustubh sawant");
+// }, 5000)
+
+// console.log(a);
+// clearTimeout(a) //stops the setTimeout and after 5sec nothing will be displayed
+
+//after 5000ms ie 5sec kaustubh sawant will be printed on console and only once
+//in console log some number appears that is the timer id
+
+// let b=setInterval(() => {
+//     console.log("kaustubh sawant");
+// }, 2000)
+// console.log(b);
+
+// clearInterval(b);//stops the execution of set interval
+
+//after 2000ms ie 2sec kaustubh sawant will be printed and continously after 2sec
+
+
+
