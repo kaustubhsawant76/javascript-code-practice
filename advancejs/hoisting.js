65475225d297e8fa860fb82e8442261b327a3033
kaustubhsawant76 <145922083+kaustubhsawant76@users.noreply.github.com>
2024-08-05 06:02:17 +0000

    hoisting

--- advancejs/hoisting.js
+++ advancejs/hoisting.js
@@ -30,3 +30,15 @@ var getName=()=>{
 // console.log(this);  //in browser it is window object  in node enviornment or vs code it is {} empty object
 //whenever you invoke a function a new execution context is created
 //lexical envornment is the local memory along with the lexical enviornment of the parent
+//let and const variables are also hoisted but they are hoisted differently as compared to var 
+//in case of let the variable is assigned memory to script and in case of var the memory assigned is global
+//allocation of memeory is called hoisting in let and const the variable is hoisted in a seperate memeory which is accesable only after initialization
+
+
+
+
+//the time between the let and const variables is hoisted and it is assigned any value is called TEMPORAL DEADZONE
+
+//INORDER TO PREVENT TEMPORAL DEADZONE ALWAYS PUT DECLARATIONS AND INITIALIZATIONS AT TOP OF THE SCOPE AS JS RUNS CODE FROM TOP TO BOTTOM
+
+//if same variable is named var outside block scope{} the value will be modified as last var declared as var leaks out of the scope and points at the same memory this concept is known as shadowing
